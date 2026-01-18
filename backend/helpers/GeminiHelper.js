c048e1c6038a2c1b51b9e09893461699069fee52
kevin-poon-cs <kevin.poon.cs@gmail.com>
2026-01-18 02:50:22 -0800

    Works but http requests with audio are still broken because of large file size.

---
+++ backend/helpers/GeminiHelper.js
@@ -0,0 +1,36 @@
+import { GoogleGenAI } from "@google/genai";
+import dotenv from "dotenv/config"
+
+export class GeminiHelper{
+    messageId = 0;
+
+    ai = new GoogleGenAI({
+        apiKey: process.env.GEMINI_API_KEY,
+    });
+
+    constructor(){
+
+    }
+
+    async send(text) {
+
+        const start = Date.now();
+
+        console.log(`${this.messageId} GeminiHelper sent ` + text);
+        const response = await this.ai.models.generateContent({
+            model: "gemini-3-flash-preview",
+            contents: text,
+        });
+
+        const end = Date.now();
+
+        console.log(`${this.messageId} GeminiHelper received ` + response.text);
+        console.log(`${this.messageId++} Time elapsed: ${end - start} ms`);
+    }
+}
+
+// Test code
+(async () => {
+    const gemini = new GeminiHelper();
+    await gemini.send("Explain how AI works in a few words");
+})();
---
+++ backend/server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import "dotenv/config";
+import {GeminiHelper} from "./helpers/GeminiHelper.js";
+import {ElevenLabsHelper} from "./helpers/ElevenLabsHelper.js";
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const elevenLabsHelper = new ElevenLabsHelper();
+const geminiHelper = new GeminiHelper();
+
+// Middleware
+app.use(express.json());
+
+// Health check
+app.get("/", (req, res) => {
+    res.send("Backend is running");
+});
+
+// Message Request
+app.get("/message", async (req, res) => {
+    const message = req.body.content;
+
+    res.json({content: await geminiHelper.send(message)});
+});
+
+// Text to Speech
+app.get("/textToSpeech", async (req, res) => {
+    const text = req.body.content;
+    res.json({content: await elevenLabsHelper.textToSpeech(text)});
+});
+
+// Speech to Text
+app.get("/SpeechToText", async (req, res) => {
+    const sound = req.body.content;
+    res.json({content: await elevenLabsHelper.speechToText(sound)});
+});
+
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+});
