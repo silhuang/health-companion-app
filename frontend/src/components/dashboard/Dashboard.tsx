31c0c51b80f7ee6662d97df2db4c791bb1d38967
Jordan So <jso01@student.ubc.ca>
2026-01-17 21:27:30 -0800

    Add layout for thoughtcards

--- frontend/src/components/dashboard/Dashboard.tsx
+++ frontend/src/components/dashboard/Dashboard.tsx
@@ -1,8 +1,44 @@
+import { useEffect, useState } from "react";
+import type { Thought } from "../../types/thought";
+import ThoughtCard from "./ThoughtCard";
+
 const Dashboard = () => {
+  const [thoughtList, setThoughtList] = useState<Thought[]>([]);
+
+  useEffect(() => {
+    // Fetch thoughts from API or local storage
+    setThoughtList([
+      {
+        title: "My First Thought",
+        content: "This is the content of my first thought.",
+        date: "2024-06-01",
+        emoji: "😊",
+      },
+      {
+        title: "Another Thought",
+        content: "Here's some more content for another thought.",
+        date: "2024-06-02",
+        emoji: "🤔",
+      },
+      {
+        title: "Another Thought",
+        content: "Here's some more content for another thought.",
+        date: "2024-06-02",
+        emoji: "🤔",
+      },
+      {
+        title: "Another Thought",
+        content: "Here's some more content for another thought.",
+        date: "2024-06-02",
+        emoji: "🤔",
+      },
+    ]);
+  }, []);
+
   return (
     <div className="flex flex-1 gap-2 bg-background">
-      <div className="flex flex-col flex-1 p-4">
-        {/* LEFT SIDE */}
+      {/* LEFT SIDE */}
+      <div className="flex flex-col gap-4 flex-1 p-4">
         <h2>Your Emoji Board</h2>
         <div
           className="rounded-4xl bg-card"
@@ -10,9 +46,18 @@ const Dashboard = () => {
         ></div>
       </div>
       {/* RIGHT SIDE */}
-      <div className="flex flex-1">
+      <div className="flex flex-col gap-4 p-4 flex-1">
         <h2>Your Thoughts</h2>
-        <div></div>
+        <div className="grid lg:grid-cols-2 xl:grid-cols-3 gap-4">
+          {thoughtList.map((thought, index) => (
+            <ThoughtCard
+              title={thought.title}
+              content={thought.content}
+              date={thought.date}
+              emoji={thought.emoji}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
