dff469afe5d964c0eaab35c44d4424ab09c03469
Jordan So <jso01@student.ubc.ca>
2026-01-18 02:36:39 -0800

    Setup react router dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router-dom";
 import Analysis from "./components/analysis/Analysis";
 import Dashboard from "./components/dashboard/Dashboard";
 import Navbar from "./components/dashboard/Navbar";
@@ -6,8 +7,10 @@ function App() {
   return (
     <div className="bg-background h-screen">
       <Navbar />
-      {/* <Dashboard /> */}
-      <Analysis />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/thought-analysis" element={<Analysis />} />
+      </Routes>
     </div>
   );
 }
