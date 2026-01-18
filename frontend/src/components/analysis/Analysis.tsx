a2823ecc2597123448904a23be4cea2c867a4051
Silvana Huang <silvanahuang23@gmail.com>
2026-01-18 11:41:57 -0800

    added icons and hover states

--- frontend/src/components/analysis/Analysis.tsx
+++ frontend/src/components/analysis/Analysis.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import AnalysisCard from "./AnalysisCard";
+import { AnalysisCard } from "./AnalysisCard";
 import {
   HeartIcon,
   LightBulbIcon,
--- frontend/src/components/analysis/AnalysisCard.tsx
+++ frontend/src/components/analysis/AnalysisCard.tsx
@@ -28,7 +28,7 @@ const COLOR_MAP = {
   },
 };
 
-const AnalysisCard = ({
+export const AnalysisCard = ({
   color,
   sentimentRating,
   sentimentLabel,
@@ -41,36 +41,41 @@ const AnalysisCard = ({
     <div
       className={`flex flex-col w-full gap-4 rounded-2xl p-12 ${COLOR_MAP[color].base}`}
     >
-    <div className={`flex flex-col w-full gap-4 rounded-2xl p-12 ${color && COLOR_MAP[color].base}`}>
-      {sentimentRating && sentimentLabel && (
-        <div className="flex gap-4 items-center">
-          <div className="w-12 h-12 flex items-center justify-center font-bold text-2xl rounded-2xl bg-card-green/90">
-            {sentimentRating}
+      <div
+        className={`flex flex-col w-full gap-4 rounded-2xl p-12 ${color && COLOR_MAP[color].base}`}
+      >
+        {sentimentRating && sentimentLabel && (
+          <div className="flex gap-4 items-center">
+            <div className="w-12 h-12 flex items-center justify-center font-bold text-2xl rounded-2xl bg-card-green/90">
+              {sentimentRating}
+            </div>
+            <div className="font-black text-2xl">{sentimentLabel}</div>
           </div>
-          <div className="font-black text-2xl">{sentimentLabel}</div>
-        </div>
-      )}
+        )}
 
-      <div className="flex gap-4 items-center">
-        <div
-          className={`w-12 h-12 flex items-center justify-center ${COLOR_MAP[color].dark} rounded-2xl`}
-        >
-          {icon &&
-            React.createElement(icon, { className: "w-8 h-8 text-text-muted" })}
+        <div className="flex gap-4 items-center">
+          <div
+            className={`w-12 h-12 flex items-center justify-center ${COLOR_MAP[color].dark} rounded-2xl`}
+          >
+            {icon &&
+              React.createElement(icon, {
+                className: "w-8 h-8 text-text-muted",
+              })}
+          </div>
+          <div className="justify-between">
+            <div className="text-2xl font-bold">{title}</div>
+            <SpeakerWaveIcon className="h-6 w-6 text-gray-500" />
+          </div>
         </div>
-        <div className="justify-between">
-          <div className="text-2xl font-bold">{title}</div>
-          <SpeakerWaveIcon className="h-6 w-6 text-gray-500" />
-        </div>
+        {content && <div className="text-xl">{content}</div>}
+        {listContent && (
+          <ul className="text-xl">
+            {listContent.map((listItem) => (
+              <li className="list-disc list-inside">{listItem}</li>
+            ))}
+          </ul>
+        )}
       </div>
-      {content && <div className="text-xl">{content}</div>}
-      {listContent && (
-        <ul className="text-xl">
-          {listContent.map((listItem) => (
-            <li className="list-disc list-inside">{listItem}</li>
-          ))}
-        </ul>
-      )}
     </div>
   );
 };
