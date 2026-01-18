9432861d51db276310d55dc0a5601d96fc6e1d60
Jordan So <jso01@student.ubc.ca>
2026-01-17 22:37:21 -0800

    Update responsive thresholds

--- frontend/src/components/dashboard/Dashboard.tsx
+++ frontend/src/components/dashboard/Dashboard.tsx
@@ -52,7 +52,7 @@ const Dashboard = () => {
       {/* RIGHT SIDE */}
       <div className="flex flex-col gap-4 p-4 flex-1">
         <div className="text-2xl font-bold">Your Thoughts</div>
-        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
           {thoughtList.map((thought, index) => (
             <ThoughtCard
               title={thought.title}
